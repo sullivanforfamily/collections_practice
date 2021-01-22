6f5ca7b48a64a2fbf469361a0b75a982dd78c6d8
charlessullivan <sullivanforfamily@gmail.com>
2021-01-21 22:33:17 -0800

    commit tree 629d468e07dd91adc8b1998a25e39b3e8aeee328

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
+def sort_array_asc(array)
+    array.sort do |a,b|
+        a<=>b
+    end
+end
+
+def sort_array_desc(array)
+    array.sort {|a,b| -(a<=>b)}
+
+end
+
+def sort_array_char_count(array)
+    array.sort.length
+end
+
+def swap_elements(array)
+    array.sort.reverse([a][b])
+end
+
+def reverse_array(array)
+    reversed = []
+    reversed << array.sort {|a,b|a<=>b}
+    reversed.reverse 
+end
+
+def kesha_maker(aray)
+
+end
+
+def find_a(array)
+    array.select 
+    newarray = newarray << if array start_with?"a" 
+end
+
+def sum_array(array)
+    arraysum=[]
+    do |array|
+    arraysum<<array.inject"+"
+    end
+    arraysum = {arraysum.sum}
+    arraysum
+end
+
+def add_s(array)
+    array.collect
+    if array != "feet" {|array| array[i ] << "s"}
+    return array
+ end
+
+
 
